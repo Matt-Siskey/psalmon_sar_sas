7d712fa8c0dc12abfd64e66173d76cf0d764ff30
Matt Siskey <matthew.siskey@dfw.wa.gov>
2026-08-17 13:01:41 -0700

    westside regional pages included
    working thru new formatting bugs from adding multiple regional pagess

--- functions/fate_plots.R
+++ functions/fate_plots.R
@@ -24,7 +24,7 @@ fate_plots <-function(fate_type, rec_type){
   rel_jday <-ggplot(data=filter(sar_df_fates, fate == fate_type & recovery_location_code %in% rec_type),aes(x=as.factor(jday),y=sar*100))+
                geom_boxplot(outlier.shape = NA)+
                geom_jitter(pch=21,size=2,width = 0.2, alpha=0.6)+
-               labs(x = "Julien Day", y = "SAR (%)", fill = "Release Site", title = paste0("Return Location: "))+
+               labs(x = "Julian Day", y = "SAR (%)", fill = "Release Site", title = paste0("Return Location: "))+
                theme(axis.text.x = element_text(angle = 45, hjust = 1))+ # Angle x-axis labels for readability
                theme(legend.position = "none")
   
